274f81af1f38d65a5e6c63d604b086c4a39ae74d
1point7point4 <1point7point4@gmail.com>
2019-11-09 14:28:38 +0100

    Set up a tiny gruesome build script

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/* This script takes heavy inspiration from:
+  https://github.com/processing/p5.js/blob/master/Gruntfile.js
+*/
+
+module.exports = (grunt) => {
+  const gruntConfig = {
+    pkg: grunt.file.readJSON('package.json'),
+    /* This minifies the javascript into a single file, and adds a banner to the
+      front of the file.
+    */
+    uglify: {
+      options: {
+        compress: {
+          global_defs: {
+            IS_MINIFIED: true
+          }
+        },
+        banner:
+          '/*! p5.js v<%= pkg.version %> <%= grunt.template.today("mmmm dd, yyyy") %> */ '
+      },
+      dist: {
+        files: {
+          'lib/jellicent.min.js': 'lib/jellicent.pre-min.js'
+        }
+      }
+    },
+  };
+
+  grunt.initConfig(gruntConfig);
+
+  // Load tasks relevant to 'build'
+  grunt.loadTasks('tasks/build');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // CURRENTLY FIXING: uglify
+
+  grunt.registerTask('build', [
+    'browserify',
+    'browserify:min',
+    'uglify',
+    'browserify:test'
+  ]);
+};
---
+++ tasks/build/browserify.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const {resolve} = require("path");
+const browserify = require("browserify");
+const {format} = require("prettier");
+const derequire = require("derequire");
+
+const filenames = {
+  min: "jellicent.pre-min.js",
+  test: "jellicent-test.js"
+};
+
+const bannerTemplate = (
+  `/*! jellicent v<%= pkg.version %> <%= grunt.template.today("mmmm dd, yyyy") %> */`
+);
+
+module.exports = function(grunt) {
+  const srcFilePath = require.resolve('../../source/index.js');
+
+  grunt.registerTask(
+    'browserify',
+    'Compile the p5.js source with Browserify',
+    function(mode) {
+      const filename = filenames[mode] || "jellicent.js";
+
+      // This file will not exist until it has been built
+      const libFilePath = resolve(`lib/${filename}`);
+
+      // Reading and writing files is asynchronous
+      const done = this.async();
+
+      // Render the banner for the top of the file
+      const banner = grunt.template.process(bannerTemplate);
+
+      // Invoke Browserify programatically to bundle the code
+      let browserified = browserify(srcFilePath, {
+        standalone: 'p5'
+      });
+
+      if (mode === "min") {
+        browserified = browserified.exclude('../../docs/reference/data.json');
+      }
+
+      const babelifyOpts = { plugins: ['static-fs'] };
+
+      if (mode === "test") {
+        babelifyOpts.envName = 'test';
+      }
+
+      const bundle = browserified.transform('babelify', babelifyOpts).bundle();
+
+      // Start the generated output with the banner comment,
+      let code = banner + '\n';
+
+      // Then read the bundle into memory so we can run it through derequire
+      bundle
+        .on('data', function(data) {
+          code += data;
+        })
+        .on('end', function() {
+
+          // "code" is complete: create the distributable UMD build by running
+          // the bundle through derequire
+          // (Derequire changes the bundle's internal "require" function to
+          // something that will not interfere with this module being used
+          // within a separate browserify bundle.)
+          code = derequire(code);
+
+          // and prettify the code
+          if (mode !== "min") {
+            code = format(code, {
+              singleQuote: true,
+              printWidth: 80 + 12,
+              parser: "babel"
+            });
+          }
+
+          // finally, write it to disk
+          grunt.file.write(libFilePath, code);
+
+          // Print a success message
+          grunt.log.writeln(
+            '>>'.green + ' Bundle ' + ('lib/' + filename).cyan + ' created.'
+          );
+
+          // Complete the task
+          done();
+        });
+    }
+  );
+};
